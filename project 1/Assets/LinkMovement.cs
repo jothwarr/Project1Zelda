e954e24f78c68b70ef1e812029ca9d47a1f42056
Joe <jothwarr@umich.edu>
2014-01-22 17:25:01 -0800

    First commit, start of link movement

---
+++ project 1/Assets/LinkMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LinkMovement : MonoBehaviour {
+	public float speed = 8;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+
+		Vector2 vel = rigidbody2D.velocity;
+		vel.x = h * speed;
+		vel.y = v * speed;
+
+		rigidbody2D.velocity = vel;
+	}
+}
