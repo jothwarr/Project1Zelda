aee13860bdbae25238a6da35d5c732b7f9e2266c
Ben Magee <benrm@umich.edu>
2014-01-26 21:47:42 -0500

    Updated camera movement

--- project 1/Assets/CameraMovement.cs
+++ project 1/Assets/CameraMovement.cs
@@ -4,17 +4,20 @@ using System.Collections;
 public class CameraMovement : MonoBehaviour {
 	Transform link;
 	Rect room;
+	Vector3 translation = new Vector3(0f, 0f, 0f);
+	static float roomWidth = 24f;
+	static float roomHeight = 12.8f;
+	static float speed = roomWidth;
 
 	// Use this for initialization
 	void Start () {
 		link = GameObject.Find ("Link").transform;
-		room = new Rect (0, 0, 12f, 12.8f);
+		room = new Rect (link.position.x - (roomWidth / 2), link.position.y + (roomHeight / 2), roomWidth, roomHeight);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Rect newRect = room;
-		Vector3 translation = new Vector3 (0f, 0f, 0f);
 		if (link.position.x > room.x + room.width) {
 			newRect.x += newRect.width;
 			translation.x += newRect.width;
@@ -32,6 +35,10 @@ public class CameraMovement : MonoBehaviour {
 			translation.y -= newRect.height;
 		}
 		room = newRect;
-		Camera.main.transform.Translate (translation);
+		Vector3 move = translation.normalized * Time.deltaTime * speed;
+		if (translation.magnitude < move.magnitude)
+						move = translation;
+		Camera.main.transform.Translate (move);
+		translation -= move;
 	}
 }
