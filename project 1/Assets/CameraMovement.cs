c1bdc0cd7307c1da8c0d111511b6dc53a6f638ff
Ben Magee <benrm@umich.edu>
2014-02-02 21:03:12 -0500

    Merged changes w/ Joe's; Moving Link between rooms not working

--- project 1/Assets/CameraMovement.cs
+++ project 1/Assets/CameraMovement.cs
@@ -1,49 +1,88 @@
 ﻿using UnityEngine;
 using System.Collections;
-using AssemblyCSharp;
 
 public class CameraMovement : MonoBehaviour {
 	Transform link;
+	//LinkMovement2 linkInstance;
 	Rect room;
 	Vector3 translation = new Vector3(0f, 0f, 0f);
-	static float roomWidth = 25.6f;
-	static float roomHeight = 17.6f;
-	static float speed = roomWidth;
+	Vector3 move = new Vector3 (0f, 0f, 0f);
+	//float linkX;
+	//float linkY;
+	const float roomWidth = 25.6f;
+	const float roomHeight = 17.6f;
+	const float speed = roomWidth;
+	const float tileWidth = 1.6f;
+	const float give = .1f;
+	bool isMoving = false;
+
+	float truncate (float input) {
+		return Mathf.Round (input * 100f) / 100f;
+	}
 
 	// Use this for initialization
 	void Start () {
 		link = GameObject.Find ("Link").transform;
-		room = new Rect (link.position.x - (roomWidth / 2), link.position.y + (roomHeight / 2), roomWidth, roomHeight);
+		//linkInstance = GameObject.Find ("Link").GetComponent<LinkMovement2> ();
+		room = new Rect (truncate(link.position.x) - (roomWidth / 2), truncate(link.position.y) + (roomHeight / 2) + (tileWidth / 2), roomWidth, roomHeight);
+		//linkX = link.position.x;
+		//linkY = link.position.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Rect newRect = room;
-		if (link.position.x > room.x + room.width) {
-			newRect.x += newRect.width;
-			translation.x += newRect.width;
+
+		//Calculate distance to next room
+		if (!isMoving) {
+			Rect newRect = room;
+			if (truncate(link.position.x) > room.x + room.width - tileWidth + give) {
+				newRect.x += newRect.width;
+				translation.x += newRect.width;
+				//linkX = link.position.x + tileWidth;
+			}
+			if (truncate(link.position.x) < room.x - give) {
+				newRect.x -= newRect.width;
+				translation.x -= newRect.width;
+				//linkX = link.position.x - tileWidth;
+			}
+			if (truncate(link.position.y) > room.y + give) {
+				newRect.y += newRect.height;
+				translation.y += newRect.height;
+				//linkY = link.position.y + tileWidth;
+			}
+			if (truncate(link.position.y) < room.y - room.height + tileWidth - give) {
+				newRect.y -= newRect.height;
+				translation.y -= newRect.height;
+				//linkY = link.position.y - tileWidth;
+			}
+			room = newRect;
 		}
-		if (link.position.x < room.x) {
-			newRect.x -= newRect.width;
-			translation.x -= newRect.width;
+
+		//Calculate distance to move this frame
+		if (translation.magnitude != 0) {
+			move = translation.normalized * Time.deltaTime * speed;
+			if (translation.magnitude < move.magnitude)
+					move = translation;
+		} else {
+			move = translation;
 		}
-		if (link.position.y > room.y) {
-			newRect.y += newRect.height;
-			translation.y += newRect.height;
+		
+		//Begin movement to other room
+		if (!PauseMovement.isTimeStopped() && move.magnitude >= give) {
+			PauseMovement.stopEverything ();
+			isMoving = true;
+			//StartCoroutine(linkInstance.MoveInGrid(linkX, linkY, link.position.z));
 		}
-		if (link.position.y < room.y - room.height) {
-			newRect.y -= newRect.height;
-			translation.y -= newRect.height;
-		}
-		room = newRect;
-		Vector3 move = translation.normalized * Time.deltaTime * speed;
-		if (translation.magnitude < move.magnitude)
-						move = translation;
-		if (move.magnitude != 0)
-						PauseMovement.stopEverything ();
+
+		//Move camera
 		Camera.main.transform.Translate (move);
 		translation -= move;
-		if (move.magnitude == 0)
-						PauseMovement.startEverything ();
+
+		//End movement to other room
+		if (PauseMovement.isTimeStopped() && translation.magnitude <= give) {
+			PauseMovement.startEverything ();
+			PauseMovement.unfreezeTime();
+			isMoving = false;
+		}
 	}
 }
