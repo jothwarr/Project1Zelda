8db901e7d096d12659919712dc63b9559b36c215
Ben Magee <benrm@umich.edu>
2014-02-06 20:46:22 -0500

    Added in XML and XML Reader

---
+++ project 1/Assets/LoadWorld.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+public class LoadWorld : MonoBehaviour {
+	World overworld;
+
+	// Use this for initialization
+	void Start () {
+		Debug.LogWarning ("Entered LoadWorld.Start()");
+		string path = Application.dataPath + "/overworld.xml";
+		XmlSerializer serializer = new XmlSerializer (typeof(World));
+		FileStream stream = new FileStream (path, FileMode.Open);
+		overworld = serializer.Deserialize (stream) as World;
+		stream.Close ();
+		Debug.LogWarning ("Read in overworld.xml");
+		Debug.LogWarning ("Number of Rooms: " + overworld.Rooms.Count);
+	}
+}
+
+[XmlRoot("World")]
+public class World {
+	[XmlArray("Rooms")]
+	[XmlArrayItem("Room")]
+	public List<Room> Rooms = new List<Room> ();
+}
+
+public class Room {
+	[XmlAttribute("x")]
+	public int x;
+
+	[XmlAttribute("y")]
+	public int y;
+
+	[XmlArray("Tiles")]
+	[XmlArrayItem("Tile")]
+	public List<Tile> Tiles = new List<Tile> ();
+}
+
+public class Tile {
+	[XmlAttribute("x")]
+	public int x;
+
+	[XmlAttribute("y")]
+	public int y;
+
+	[XmlAttribute("val")]
+	public string value;
+}
